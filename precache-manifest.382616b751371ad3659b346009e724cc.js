fc27c87843001a20cf5d35688a88feea2dd12c7a
umar-hasan <umarhasan16@gmail.com>
2020-09-02 14:38:26 -0400

    Updates

---
+++ precache-manifest.382616b751371ad3659b346009e724cc.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "812cbfcb8e51f01c269d035ef7f9f88d",
+    "url": "/FlowerMan/index.html"
+  },
+  {
+    "revision": "c774fe1f624833549932",
+    "url": "/FlowerMan/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "9c625f4e7a5b2a68deda",
+    "url": "/FlowerMan/static/css/main.8437a7a2.chunk.css"
+  },
+  {
+    "revision": "c774fe1f624833549932",
+    "url": "/FlowerMan/static/js/2.b59e6084.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/FlowerMan/static/js/2.b59e6084.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c625f4e7a5b2a68deda",
+    "url": "/FlowerMan/static/js/main.7968caab.chunk.js"
+  },
+  {
+    "revision": "2c328366c61493542b82",
+    "url": "/FlowerMan/static/js/runtime-main.f428282c.js"
+  },
+  {
+    "revision": "b384730564936798bd2c87074d2bb2bb",
+    "url": "/FlowerMan/static/media/Logo.b3847305.png"
+  },
+  {
+    "revision": "dd7e295e9faa99f4a691448d0f4a8a29",
+    "url": "/FlowerMan/static/media/bridal-stage.dd7e295e.jpg"
+  },
+  {
+    "revision": "1fc1da6d1dd2651af12c3d04f4f74a6d",
+    "url": "/FlowerMan/static/media/contact_flowers.1fc1da6d.jpg"
+  },
+  {
+    "revision": "f206c80bbe04441d99a00bf4376c9ae3",
+    "url": "/FlowerMan/static/media/design1.f206c80b.jpg"
+  },
+  {
+    "revision": "1c208a49bd1acac315b214da9385a597",
+    "url": "/FlowerMan/static/media/design2.1c208a49.jpg"
+  },
+  {
+    "revision": "fdac3559f5ff62c245eecd3c209df1a2",
+    "url": "/FlowerMan/static/media/design3.fdac3559.jpg"
+  },
+  {
+    "revision": "2717f3a0e7c16a4f1a2e5deb3813f673",
+    "url": "/FlowerMan/static/media/engagements.2717f3a0.jpg"
+  },
+  {
+    "revision": "47293fa144692fff951e1109510bb905",
+    "url": "/FlowerMan/static/media/event1.47293fa1.jpg"
+  },
+  {
+    "revision": "78cbcf7ca0f2d549e19bddb3bae6ec75",
+    "url": "/FlowerMan/static/media/event2.78cbcf7c.jpg"
+  },
+  {
+    "revision": "0b4e88dc70a9b6911db561e40101a384",
+    "url": "/FlowerMan/static/media/event3.0b4e88dc.jpg"
+  },
+  {
+    "revision": "906bd7938641b15650df7bb12c0497e6",
+    "url": "/FlowerMan/static/media/flower_carousel_1.906bd793.jpg"
+  },
+  {
+    "revision": "08a5ec846b1016ac05c3c1f0bf054af0",
+    "url": "/FlowerMan/static/media/flower_carousel_2.08a5ec84.jpg"
+  },
+  {
+    "revision": "b96f799ffc21d410c1a955b92188d435",
+    "url": "/FlowerMan/static/media/flower_carousel_3.b96f799f.jpg"
+  },
+  {
+    "revision": "30c3847fa94b6a0cc7e8bbbc87a4b235",
+    "url": "/FlowerMan/static/media/flowers1.30c3847f.jpg"
+  },
+  {
+    "revision": "6ef8f6c87f3138528189a1b6648ec136",
+    "url": "/FlowerMan/static/media/flowers2.6ef8f6c8.jpg"
+  },
+  {
+    "revision": "c8a77e4d301a90a50de7b4df9a085fff",
+    "url": "/FlowerMan/static/media/instagram.c8a77e4d.png"
+  },
+  {
+    "revision": "1e52dd75b3151a2ae6b451a02ac16a10",
+    "url": "/FlowerMan/static/media/intro.1e52dd75.mov"
+  },
+  {
+    "revision": "2430c14438f0b8b93b43959d71ac48a4",
+    "url": "/FlowerMan/static/media/pinterest.2430c144.png"
+  },
+  {
+    "revision": "236de94b2db38ce8550dcd9b44c65973",
+    "url": "/FlowerMan/static/media/profile.236de94b.jpg"
+  },
+  {
+    "revision": "b72b3e92742b586c5e3e094e02388923",
+    "url": "/FlowerMan/static/media/services.b72b3e92.jpg"
+  },
+  {
+    "revision": "d535fadef2251db059a9ba31929b920a",
+    "url": "/FlowerMan/static/media/shop.d535fade.jpg"
+  },
+  {
+    "revision": "1d590631ca96db243670d86353a96743",
+    "url": "/FlowerMan/static/media/special.1d590631.jpg"
+  },
+  {
+    "revision": "41f2029b06d4eb8f0ac875311401705a",
+    "url": "/FlowerMan/static/media/tabletop.41f2029b.jpg"
+  }
+]);
